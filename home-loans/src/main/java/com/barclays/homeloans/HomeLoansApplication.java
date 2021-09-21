194c0a1105249d071ed11179967c99c6de9e58ab
Sanya Varghese <varghese.sanya2gmail.com>
2021-09-21 07:14:01 +0530

    Initial Commit

---
+++ home-loans/src/main/java/com/barclays/homeloans/HomeLoansApplication.java
@@ -0,0 +1,13 @@
+package com.barclays.homeloans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeLoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeLoansApplication.class, args);
+	}
+
+}
---
+++ home-loans/src/test/java/com/barclays/homeloans/HomeLoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barclays.homeloans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeLoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
